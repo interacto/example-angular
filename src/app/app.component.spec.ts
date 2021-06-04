f8085555cd9b0dae678b8659f431c137e52393ce
Rémi Daniel <remi.daniel@insa-rennes.fr>
2021-06-04 18:09:13 +0200

    test(text): Added tests for bindings related to the text field.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,14 @@
-import { TestBed, waitForAsync } from '@angular/core/testing';
+import {fakeAsync, TestBed, tick, waitForAsync} from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import {BrowserModule} from '@angular/platform-browser';
+import {BrowserModule, By} from '@angular/platform-browser';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {DragDropModule} from '@angular/cdk/drag-drop';
 import {InteractoModule} from 'interacto-angular';
+import {robot} from './StubEvents';
+
+let app: AppComponent;
+let fixture;
 
 describe('AppComponent', () => {
   beforeEach(waitForAsync(() => {
@@ -20,11 +24,31 @@ describe('AppComponent', () => {
         InteractoModule
       ]
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  // Text area
+  it('should clear the text area', () => {
+    const textField: HTMLTextAreaElement = fixture.debugElement.query(By.css('textArea')).nativeElement;
+    const clearTextButton: HTMLButtonElement = fixture.debugElement.query(By.css('.clearTextButton')).nativeElement;
+    robot(clearTextButton).click();
+    fixture.detectChanges(); // Updates the page so that the new text is displayed
+    expect(app.dataService.txt).toEqual('');
+    expect(textField.value).toEqual('');
+  });
+
+  it('should edit the text area', fakeAsync(() => {
+    const textField: HTMLTextAreaElement = fixture.debugElement.query(By.css('textArea')).nativeElement;
+    textField.value = 'some text';
+    robot(textField).input();
+    tick(1000);
+    expect(textField.value ).toEqual('some text');
+    expect(app.dataService.txt).toEqual('some text');
+  }));
 });
