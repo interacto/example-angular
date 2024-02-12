7b35b0bf1374be8c8775c0ee138214d508035826
arnobl <1052622+arnobl@users.noreply.github.com>
2024-02-12 16:54:55 +0100

    using angular 17 loops

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -21,18 +21,21 @@ export class DataService {
     this.cards2 = [];
 
     this.cards1.push({
+      id: 1,
       title: 'card 1',
       subTitle: 'The card 1',
       text: 'Some text for card 1'
     });
 
     this.cards1.push({
+      id: 2,
       title: 'card 2',
       subTitle: 'The card 2',
       text: 'Some text for card 2'
     });
 
     this.cards2.push({
+      id: 3,
       title: 'card 3',
       subTitle: 'The card 3',
       text: 'Some text for card 3'
@@ -41,6 +44,7 @@ export class DataService {
 }
 
 export interface CardData {
+  id: number;
   title: string;
   subTitle: string;
   text: string;
